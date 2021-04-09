09c57a664f9c6df1cc6b8af1351561e6146ed61d
Christopher Girvin <chrisg8691@gmail.com>
2021-04-08 22:26:03 -0400

    works, but can be improved; eyes hurt, need a short break; probably not the most efficient; at an avg of like 990 moves, but I can do better

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,9 +5,6 @@ from world import World
 import random
 from ast import literal_eval
 
-# my imports
-import collections
-
 # Load world
 world = World()
 
@@ -16,8 +13,8 @@ world = World()
 # map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -30,7 +27,121 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+
+# Solution
 traversal_path = []
+traversal_graph = {}
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return
+
+    def size(self):
+        return len(self.queue)
+
+def path_traverser():
+    global traversal_path
+
+    while True:
+        next_move = try_neighbors()
+
+        while next_move:
+            traversal_path.append(next_move)
+            next_move = try_neighbors()
+
+        nearest_empty_path = find_nearest_empty_path(player.current_room)
+
+        if nearest_empty_path is None:
+            break
+
+        traversal_path += nearest_empty_path
+
+        for direction in nearest_empty_path:
+            player.travel(direction)
+
+
+def try_neighbors():
+    global traversal_graph
+    directions = {"n", "s", "e", "w"}
+    next_direction = None
+
+    for direction in directions:
+        traversal_graph.setdefault(player.current_room.id, {'n': '?', 's': '?', 'e': '?', 'w': '?'})
+        if traversal_graph[player.current_room.id][direction] is not "?":
+            continue
+
+        last_room = player.current_room
+        player.travel(direction)
+
+        if update_rooms(direction, last_room, player.current_room):
+            next_direction = direction
+            player.travel(get_opposite_direction(direction))
+
+    if next_direction:
+        player.travel(next_direction)
+
+    return next_direction
+
+
+def update_rooms(direction, last_room, current_room):
+    global traversal_graph
+
+    if last_room is current_room:
+        traversal_graph[current_room.id][direction] = None
+        return False
+
+    else:
+        traversal_graph.setdefault(current_room.id, {'n': '?', 's': '?', 'e': '?', 'w': '?'})
+        traversal_graph[last_room.id][direction] = current_room.id
+        traversal_graph[current_room.id][get_opposite_direction(direction)] = last_room.id
+        return True
+
+
+def get_opposite_direction(direction):
+    if direction is "s":
+        return "n"
+    elif direction is "n":
+        return "s"
+    elif direction is "e":
+        return "w"
+    elif direction is "w":
+        return "e"
+    else:
+        return
+
+
+def find_nearest_empty_path(current):
+    global traversal_graph
+    queue = Queue()
+    queue.enqueue(current.id)
+    visited = {}
+
+    while queue.size() > 0:
+        room = queue.dequeue()
+        visited.setdefault(room, [])
+
+        for (key, value) in traversal_graph[room].items():
+            if value is None:
+                continue
+
+            if value is '?':
+                return visited[room]
+            else:
+                if value not in visited:
+                    visited.setdefault(value, visited[room].copy())
+                    visited[value].append(key)
+                    queue.enqueue(value)
+
+    return
 
 # bfs from yesterday, add weighted edges? fucking dijkstra
 # though, I probably cannot edit the graph itself
@@ -64,6 +175,7 @@ traversal_path = []
     # return
 
 # TRAVERSAL TEST
+path_traverser()
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
